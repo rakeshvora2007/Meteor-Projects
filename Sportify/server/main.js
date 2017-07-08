b51dc04b65c766d50085e92e0a30093ad2072ca7
Rakesh Jain <rakeshvora2007@gmail.com>
2017-07-07 23:12:53 -0500

    deleted clutters

--- Sportify/server/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { CreateGame } from '../imports/api/createGame';
-
-Meteor.startup(() => {
-    Meteor.publish('createGame', function() {
-        return CreateGame.find({}).fetch();
-    })
-});
